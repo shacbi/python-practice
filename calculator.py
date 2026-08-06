0d426894d06dc04867d91cbb786ec15499604e44
ABDUL RAHMAN <shaxyardls@gmail.com>
2026-08-07 06:43:21 +1245

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Simple Calculator
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
