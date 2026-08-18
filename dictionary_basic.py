5a684a9785a0c37307fea8d562f1e42f15e4b75e
ABDUL RAHMAN <shaxyardls@gmail.com>
2026-08-18 18:08:23 +1000

    Add files via upload

---
+++ dictionary_basic.py
@@ -0,0 +1,20 @@
+# Python Dictionary Basics
+
+student = {
+    "name": "Shacbi",
+    "age": 20,
+    "language": "Python"
+}
+
+# Get a value
+print(student["name"])
+print(student["age"])
+print(student["language"])
+
+# Change a value
+student["age"] = 21
+print(student)
+
+# Add a new key and value
+student["country"] = "Somalia"
+print(student)
